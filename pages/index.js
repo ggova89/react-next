508c236f5b097a073cb860d29cf0e08620d8e486
ggova <ggova@naver.com>
2021-01-08 10:28:37 +0900

    react-next
    commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import Axios from "axios";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { Divider, Header, Loader } from "semantic-ui-react";
+import ItemList from "../src/component/ItemList";
+import styles from "../styles/Home.module.css";
+
+export default function Home({ list }) {
+  return (
+    <div>
+      <Head>
+        <title>HOME | 코딩앙마</title>
+        <meta name="description" content="코딩 앙마 홈입니다."></meta>
+      </Head>
+      <>
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          베스트 상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(0, 9)} />
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          신상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(9)} />
+      </>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const apiUrl = process.env.apiUrl;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
+}
